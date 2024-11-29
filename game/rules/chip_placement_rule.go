7e3e7d6b514201ada97862f5a61a224ff7aec1f6
mohammed.r.uddin <mohammed.r.uddin@accenture.com>
2024-11-29 10:52:44 +0700

    add chip test

--- game/rules/chip_placement_rule.go
+++ game/rules/chip_placement_rule.go
@@ -1,27 +1,28 @@
 package rules
 
 import (
+	"A-MATH/err"
 	"A-MATH/game/components"
 	"A-MATH/game/constants"
 )
 
-func IsChipPlaceCorrectly(board components.Board, coordinate [][2]int) {
-	if !IsChipPlaceOnCenterSquare(coordinate) && board.IsEmpty() {
+func IsChipPlaceCorrectly(board components.Board, coordinates [][2]int) {
+	if !IsChipPlaceOnCenterSquare(coordinates) && board.IsEmpty() {
 		return //add error
 	}
 
-	if IsChipsPlacedOnOccupiedSquare(board, coordinate) {
+	if IsChipsPlacedOnOccupiedSquare(board, coordinates) {
 		return //add error
 	}
 
-	isVertical, isHorizontal := IsChipPlaceOnVerticalOrHorizontal(coordinate)
+	isVertical, isHorizontal := IsChipPlaceOnVerticalOrHorizontal(coordinates)
 	if !isVertical && !isHorizontal {
 		return //add error
 	}
 }
 
-func IsChipPlaceOnCenterSquare(coordinate [][2]int) bool {
-	for _, co := range coordinate {
+func IsChipPlaceOnCenterSquare(coordinates [][2]int) bool {
+	for _, co := range coordinates {
 		if co == [2]int{8, 8} {
 			return true
 		}
@@ -29,16 +30,16 @@ func IsChipPlaceOnCenterSquare(coordinate [][2]int) bool {
 	return false
 }
 
-func IsChipPlaceOnVerticalOrHorizontal(coordinate [][2]int) (bool, bool) {
-	if len(coordinate) == 1 {
+func IsChipPlaceOnVerticalOrHorizontal(coordinates [][2]int) (bool, bool) {
+	if len(coordinates) == 1 {
 		return true, true
 	}
 
 	isVertical := true
 	isHorizontal := true
-	firstRow, firstColumn := coordinate[0][0], coordinate[0][1]
+	firstRow, firstColumn := coordinates[0][0], coordinates[0][1]
 
-	for _, co := range coordinate[1:] {
+	for _, co := range coordinates[1:] {
 		if co[0] != firstRow {
 			isVertical = false
 		}
@@ -56,8 +57,8 @@ func IsChipPlaceOnVerticalOrHorizontal(coordinate [][2]int) (bool, bool) {
 	return isVertical, isHorizontal
 }
 
-func IsChipsPlacedOnOccupiedSquare(board components.Board, coordinate [][2]int) bool {
-	for _, co := range coordinate {
+func IsChipsPlacedOnOccupiedSquare(board components.Board, coordinates [][2]int) bool {
+	for _, co := range coordinates {
 		if board.GetSquare(co).HasChipPlacedOn() {
 			return true
 		}
@@ -65,16 +66,68 @@ func IsChipsPlacedOnOccupiedSquare(board components.Board, coordinate [][2]int) 
 	return false
 }
 
-func ChipPlacementConnector(board components.Board, coordinate [][2]int, isVertical, isHorizontal bool) {
-	if len(coordinate) == 0 {
+func IsChipPlacementSeperated(board components.Board, coordinates [][2]int, isVertical, isHorizontal bool) (bool, error) {
+	if len(coordinates) < 2 {
+		return false, nil // Cannot be separated if less than two chips
+	}
+
+	checkGap := func(start, end, fixedCoord int, isVertical bool) error {
+		for i := start + 1; i < end; i++ {
+			pos := [2]int{}
+			if isVertical {
+				pos = [2]int{fixedCoord, i}
+			} else {
+				pos = [2]int{i, fixedCoord}
+			}
+
+			if !board.GetSquare(pos).HasChipPlacedOn() {
+				return err.New(int(constants.BadRequest), string(constants.InvalidChipPlacement))
+			}
+		}
+		return nil
+	}
+
+	var isSeparationDetected = false
+
+	if isVertical {
+		for i := 0; i < len(coordinates)-1; i++ {
+			start, end := coordinates[i][1], coordinates[i+1][1]
+			if start+1 != end { // Check for separation
+				isSeparationDetected = true
+				if err := checkGap(start, end, coordinates[i][0], true); err != nil {
+					return false, err
+				}
+			}
+		}
+		return isSeparationDetected, nil
+	}
+
+	if isHorizontal {
+		for i := 0; i < len(coordinates)-1; i++ {
+			start, end := coordinates[i][0], coordinates[i+1][0]
+			if start+1 != end { // Check for separation
+				isSeparationDetected = true
+				if err := checkGap(start, end, coordinates[i][1], false); err != nil {
+					return false, err
+				}
+			}
+		}
+		return isSeparationDetected, nil
+	}
+
+	return false, nil
+}
+
+func ChipPlacementConnector(board components.Board, coordinates [][2]int, isVertical, isHorizontal bool) {
+	if len(coordinates) == 0 {
 		return
 	}
-	if len(coordinate) == 1 {
+	if len(coordinates) == 1 {
 		return
 	}
 
-	edgeConnectorSet := EdgeConnector(board, coordinate, isVertical, isHorizontal)
-	crossConnectorSet := CrossConnector(board, coordinate, isVertical, isHorizontal)
+	edgeConnectorSet := EdgeConnector(board, coordinates, isVertical, isHorizontal)
+	crossConnectorSet := CrossConnector(board, coordinates, isVertical, isHorizontal)
 
 	if len(edgeConnectorSet) == 0 {
 		return
@@ -85,42 +138,42 @@ func ChipPlacementConnector(board components.Board, coordinate [][2]int, isVerti
 
 }
 
-func SingleChipConnector(board components.Board, coordinate [2]int) [][2]int {
+func SingleChipConnector(board components.Board, coordinates [2]int) [][2]int {
 	var connectors [][2]int
 
 	directions := [][2]int{{-1, 0}, {1, 0}, {0, -1}, {0, 1}}
-	addDirectionalConnectors(board, coordinate[0], coordinate[1], directions, &connectors)
+	addDirectionalConnectors(board, coordinates[0], coordinates[1], directions, &connectors)
 
 	return connectors
 }
 
-func PrefixSuffixConnector(board components.Board, coordinate [][2]int, isVertical, isHorizontal bool) [][2]int {
+func PrefixSuffixConnector(board components.Board, coordinates [][2]int, isVertical, isHorizontal bool) [][2]int {
 	var connectors [][2]int
 
 	if isVertical {
-		addConnector(board, coordinate[0][0], coordinate[0][1]-1, &connectors)
-		addConnector(board, coordinate[len(coordinate)-1][0], coordinate[len(coordinate)-1][1]+1, &connectors)
+		addConnector(board, coordinates[0][0], coordinates[0][1]-1, &connectors)
+		addConnector(board, coordinates[len(coordinates)-1][0], coordinates[len(coordinates)-1][1]+1, &connectors)
 	}
 
 	if isHorizontal {
-		addConnector(board, coordinate[0][0]-1, coordinate[0][1], &connectors)
-		addConnector(board, coordinate[len(coordinate)-1][0]+1, coordinate[len(coordinate)-1][1], &connectors)
+		addConnector(board, coordinates[0][0]-1, coordinates[0][1], &connectors)
+		addConnector(board, coordinates[len(coordinates)-1][0]+1, coordinates[len(coordinates)-1][1], &connectors)
 	}
 
 	return connectors
 }
 
-func EdgeConnector(board components.Board, coordinate [][2]int, isVertical, isHorizontal bool) [][2]int {
+func EdgeConnector(board components.Board, coordinates [][2]int, isVertical, isHorizontal bool) [][2]int {
 	var connectors [][2]int
 
 	if isVertical {
-		for _, co := range coordinate {
+		for _, co := range coordinates {
 			addDirectionalConnectors(board, co[0], co[1], [][2]int{{-1, 0}, {1, 0}}, &connectors)
 		}
 	}
 
 	if isHorizontal {
-		for _, co := range coordinate {
+		for _, co := range coordinates {
 			addDirectionalConnectors(board, co[0], co[1], [][2]int{{0, -1}, {0, 1}}, &connectors)
 		}
 	}
@@ -128,21 +181,21 @@ func EdgeConnector(board components.Board, coordinate [][2]int, isVertical, isHo
 	return connectors
 }
 
-func CrossConnector(board components.Board, coordinate [][2]int, isVertical, isHorizontal bool) [][2]int {
+func CrossConnector(board components.Board, coordinates [][2]int, isVertical, isHorizontal bool) [][2]int {
 	var connectors [][2]int
 
 	if isVertical {
-		for i := 0; i < len(coordinate)-1; i++ {
-			if coordinate[i][1]+1 != coordinate[i+1][1] {
-				connectors = append(connectors, [2]int{coordinate[i][0], coordinate[i][1] + 1})
+		for i := 0; i < len(coordinates)-1; i++ {
+			if coordinates[i][1]+1 != coordinates[i+1][1] {
+				connectors = append(connectors, [2]int{coordinates[i][0], coordinates[i][1] + 1})
 			}
 		}
 	}
 
 	if isHorizontal {
-		for i := 0; i < len(coordinate)-1; i++ {
-			if coordinate[i][0]+1 != coordinate[i+1][0] {
-				connectors = append(connectors, [2]int{coordinate[i][0] + 1, coordinate[i][1]})
+		for i := 0; i < len(coordinates)-1; i++ {
+			if coordinates[i][0]+1 != coordinates[i+1][0] {
+				connectors = append(connectors, [2]int{coordinates[i][0] + 1, coordinates[i][1]})
 			}
 		}
 	}
---
+++ tests/game/components/chip_test.go
@@ -0,0 +1,59 @@
+package components_test
+
+import (
+	"A-MATH/game/components"
+	"A-MATH/game/constants"
+	"testing"
+)
+
+func TestNewChip(t *testing.T) {
+	tests := []struct {
+		name             string
+		value            string
+		expectedScore    int
+		expectedChipType string
+	}{
+		{"Chip: Zero", string(constants.Zero), 1, string(constants.OneDigitNumberType)},
+		{"Chip: One", string(constants.One), 1, string(constants.OneDigitNumberType)},
+		{"Chip: Two", string(constants.Two), 1, string(constants.OneDigitNumberType)},
+		{"Chip: Three", string(constants.Three), 1, string(constants.OneDigitNumberType)},
+		{"Chip: Four", string(constants.Four), 2, string(constants.OneDigitNumberType)},
+		{"Chip: Five", string(constants.Five), 2, string(constants.OneDigitNumberType)},
+		{"Chip: Six", string(constants.Six), 2, string(constants.OneDigitNumberType)},
+		{"Chip: Seven", string(constants.Seven), 2, string(constants.OneDigitNumberType)},
+		{"Chip: Eight", string(constants.Eight), 2, string(constants.OneDigitNumberType)},
+		{"Chip: Nine", string(constants.Nine), 2, string(constants.OneDigitNumberType)},
+		{"Chip: Ten", string(constants.Ten), 3, string(constants.TwoDigitNumberType)},
+		{"Chip: Eleven", string(constants.Eleven), 4, string(constants.TwoDigitNumberType)},
+		{"Chip: Twelve", string(constants.Twelve), 3, string(constants.TwoDigitNumberType)},
+		{"Chip: Thirteen", string(constants.Thirteen), 6, string(constants.TwoDigitNumberType)},
+		{"Chip: Fourteen", string(constants.Fourteen), 4, string(constants.TwoDigitNumberType)},
+		{"Chip: Fifteen", string(constants.Fifteen), 4, string(constants.TwoDigitNumberType)},
+		{"Chip: Sixteen", string(constants.Sixteen), 4, string(constants.TwoDigitNumberType)},
+		{"Chip: Seventeen", string(constants.Seventeen), 6, string(constants.TwoDigitNumberType)},
+		{"Chip: Eighteen", string(constants.Eighteen), 4, string(constants.TwoDigitNumberType)},
+		{"Chip: Nineteen", string(constants.Nineteen), 7, string(constants.TwoDigitNumberType)},
+		{"Chip: Twenty", string(constants.Twenty), 5, string(constants.TwoDigitNumberType)},
+		{"Chip: Addition", string(constants.Addition), 2, string(constants.OperatorType)},
+		{"Chip: Subtraction", string(constants.Subtraction), 2, string(constants.OperatorType)},
+		{"Chip: Multiply", string(constants.Multiply), 2, string(constants.OperatorType)},
+		{"Chip: Division", string(constants.Division), 2, string(constants.OperatorType)},
+		{"Chip: Add_sub", string(constants.Add_sub), 1, string(constants.AlterOperatorType)},
+		{"Chip: Multi_divide", string(constants.Multi_divide), 1, string(constants.AlterOperatorType)},
+		{"Chip: Equal", string(constants.Equal), 1, string(constants.EqualType)},
+		{"Chip: Blank", string(constants.Blank), 0, string(constants.BlankType)},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			result := components.NewChip(tt.value)
+			if result.Score != tt.expectedScore {
+				t.Errorf("NewChip(%s) = Score:%d; want %d", tt.value, result.Score, tt.expectedScore)
+			}
+
+			if result.ChipType != tt.expectedChipType {
+				t.Errorf("NewChip(%s) = Chip Type:%s; want %s", tt.value, result.ChipType, tt.expectedChipType)
+			}
+		})
+	}
+}
